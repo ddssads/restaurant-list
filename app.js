6bbef6e4b4f90211d33fbee73010aba918217124
ddssads <tw0937608360@gmail.com>
2020-12-16 18:26:11 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
@@ -10,6 +11,16 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 //setting route
 app.get('/', (req, res) => {
