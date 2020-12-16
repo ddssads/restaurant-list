aad03fc70f1c8c48338c0dec9d9e64f4c4c51a39
ddssads <tw0937608360@gmail.com>
2020-12-16 21:50:16 +0800

    feat: restaurant CRUD function

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const restaurant = require('./models/restaurant')
 const app = express()
 const port = 3000
 
@@ -22,19 +24,63 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 //setting route
+//Show index page
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
-//Show index page
+//create restaurant
+app.get('/restaurants/create', (req, res) => {
+  return res.render('create')
+})
+
+app.post('/restaurants', (req, res) => {
+  return Restaurant.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+//Show restaurant detail
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(id)
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+//edit page
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const newRestaurant = req.body
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant = Object.assign(restaurant, newRestaurant)
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
+//delete 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
