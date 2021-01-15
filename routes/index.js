9b3c95f36bba21965bff36bd8e65091769f68cd0
ddssads <tw0937608360@gmail.com>
2021-01-15 12:26:10 +0800

    feat: add user modules & login register page

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
+const user = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
 router.use('/sort', sort)
-
+router.use('/users', user)
 module.exports = router
