f7757c2a35807bbf4cbbaecc12bed08538f29dc2
ddssads <tw0937608360@gmail.com>
2021-01-16 11:36:38 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const routes = require('./routes')
 const usePassport = require('./config/passport')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 require('./config/mongoose')
 
@@ -17,7 +20,7 @@ app.set('view engine', 'handlebars')
 
 
 app.use(session({
-  secret: 'Abcskjdd',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
 const user = require('./modules/users')
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/restaurants', authenticator, restaurants)
 router.use('/search', authenticator, search)
 router.use('/sort', authenticator, sort)
 router.use('/users', user)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 module.exports = router
